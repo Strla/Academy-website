2f702370e5839e811077a10c35cc0509044b7acd
Ivan Starčević <ivan.starcevic15@gmail.com>
2022-05-01 19:42:55 +0200

    Complete lecture7 hands on

---
+++ src/components/Button/Button.js
@@ -0,0 +1,18 @@
+import "./Button.scss";
+
+const Button = ({ modifiers, children }) => {
+  const modifierClasses = {
+    secondary: "Button-Secondary",
+    nav: "Button-Nav",
+    landing: "Button-Landing",
+    heading: "Button-Heading",
+    outline: "Button-Outline",
+  };
+
+  let buttonClass = "Button";
+
+  modifiers.map((modifier) => (buttonClass += " " + modifierClasses[modifier]));
+  return <button className={buttonClass}>{children}</button>;
+};
+
+export default Button;
